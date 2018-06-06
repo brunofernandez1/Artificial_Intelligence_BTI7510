4558765c23506986f8f98a14c00bdcd7a93947d1
Bruno Fernandez <brunofernandez@msn.com>
2018-06-06 18:37:53 +0200

    working search with key

--- myfirstprolog.pl
+++ myfirstprolog.pl
@@ -1,21 +1,24 @@
-mensch(du).
-mensch(ich).
-mensch(sie).
-mensch(es).
-liebt(ich, X) :- liebt(X, du), mensch(X).
-liebt(sie, du).
-liebt(es, du).
+tuer(a,b).
+tuer(b,c).
+%tuer(d,e).
+tuer(c,d).
+tuer(e,f).
+tuer(g,e).
 
-textsame(T1, T2) :- T1 == T2.
-textnotsame(T1, T2) :- T1 \= T2.
+key(d,b,e).
+keypfeil(X,Y,Z):-key(X,Y,Z).
+keypfeil(X,Y,Z):-key(X,Z,Y).
 
-pfeil(a,b).
-pfeil(a,c).
-pfeil(a,d).
-pfeil(b,c).
-pfeil(c,d).
-pfeil(c,e).
-pfeil(d,e).
+pfeil(X,Y):-tuer(X,Y).
+pfeil(X,Y):-tuer(Y,X).
 
-weg(X,Y):-pfeil(X,Y).
-weg(X,Y):-pfeil(X,Z),weg(Z,Y).
+tiefensuche(Start,Ziel,Loesung):-pfeil(Start,_),pfeil(_,Ziel),
+                                 tiefensuche(Start,[Start],Ziel,Loesung).
+
+tiefensuche(Ziel,Pfad,Ziel,Loesung):-Loesung = Pfad,!.
+tiefensuche(KnotenA,Pfad,Ziel,Loesung):-((pfeil(KnotenA,KnotenN),
+                                        not(member(KnotenN,Pfad)));
+										(keypfeil(KeyKnoten,KnotenA, KnotenN),
+										member(KeyKnoten,Pfad))),
+                                        append(Pfad,[KnotenN],PfadNeu),
+                                        tiefensuche(KnotenN,PfadNeu,Ziel,Loesung).
