4e3b80861c2cd5f2649d7870f4c6a9ff8a25dded
Claudio Polo <claudiodano.polo@students.bfh.ch>
2018-06-06 13:19:22 +0200

    change connected

--- labyrinth.pl
+++ labyrinth.pl
@@ -36,45 +36,44 @@ door(door13).
 door(door14).
 door(door15).
 
-
 %the connections between doors and rooms
-connected(start, door0, room1).
-connected(room1, door0, start).
-connected(room1, door1, room2).
-connected(room2, door1, room1).
-connected(room1, door2, room6).
-connected(room3, door10, room4).
-connected(room3, door11, room5).
-connected(room4, door10, room3).
-connected(room4, door9, room7).
-connected(room5, door11, room3).
-connected(room6, door2, room1).
-connected(room6, door3, room8).
-connected(room6, door5, room9).
-connected(room6, door6, room10).
-connected(room7, door8, room11).
-connected(room7, door9, room4).
-connected(room8, door3, room6).
-connected(room8, door4, room9).
-connected(room9, door4, room8).
-connected(room9, door5, room6).
-connected(room10, door6, room6).
-connected(room10, door7, room12).
-connected(room11, door8, room7).
-connected(room12, door7, room10).
-connected(room12, door12, room13).
-connected(room13, door12, room12).
-connected(room13, door13, room14).
-connected(room14, door13, room13).
-connected(room14, door14, room15).
-connected(room15, door14, room14).
-connected(room15, door15, end).
-%connected(end, door15, room15).
-
+connected(room1, door0).
+connected(room1, door1).
+connected(room1, door2).
+connected(room2, door1).
+connected(room3, door10).
+connected(room3, door11).
+connected(room4, door10).
+connected(room4, door9).
+connected(room5, door11).
+connected(room6, door2).
+connected(room6, door3).
+connected(room6, door5).
+connected(room6, door6).
+connected(room7, door8).
+connected(room7, door9).
+connected(room8, door3).
+connected(room8, door4).
+connected(room9, door4).
+connected(room9, door5).
+connected(room10, door6).
+connected(room10, door7).
+connected(room11, door8).
+connected(room12, door7).
+connected(room12, door12).
+connected(room13, door12).
+connected(room13, door13).
+connected(room14, door13).
+connected(room14, door14).
+connected(room15, door14).
+connected(room15, door15).
+connected(end, door15).
+connected(start, door0).
 
 
 %get all possible moves to go
-connected(X, Y, Z) :- room(X), door(Y), room(Z).
+connected(X, Y) :- room(X).
+connected(X, Y) :- door(Y).
 
 %all persons in game
 person(agent).
@@ -82,26 +81,13 @@ person(guard1).
 person(guard2).
 
 %current position of agent
-currentPos(agent, start).
+currentPos(agent, room0).
 currentPos(guard1, room5).
 
 %function to move arond
-%move(X, Y, Z) :- room(X), door(Y), room(Z), connected(Z, Y).
+move(X, Y, Z) :- room(X), door(Y), room(Z), connected(Z, Y).
 
-%fuction to find doors
-look(X, Z):- connected(X, Y, Z).
-
-weg(X,Z):-connected(X,Y,Z).
-%weg(X,Y):-connected(X,Y,Z),weg(L,Y).
-
-tiefensuche(Start,Ziel,Loesung):-connected(Start,_,Ziel),connected(_,Ziel),
-                                 tiefensuche(Start,[Start],Ziel,Loesung).
-								 
-tiefensuche(Ziel,Pfad,Ziel,Loesung):-Loesung = Pfad,!.
-tiefensuche(KnotenA,Pfad,Ziel,Loesung):-connected(KnotenA,KnotenN),
-                                        not(member(KnotenN,Pfad)),
-                                        append(Pfad,[KnotenN],PfadNeu),
-                                        tiefensuche(KnotenN,PfadNeu,Ziel,Loesung).
+giveafuck(X,Z) :- connected(X,Y), connected(Z,Y).
 
 
 
