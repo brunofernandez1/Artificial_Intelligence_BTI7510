fb2f68b3411562f9e5b2545de84ec2028df53115
Bruno Fernandez <brunofernandez@msn.com>
2018-06-06 13:05:03 +0200

    some changes

--- myfirstprolog.pl
+++ myfirstprolog.pl
@@ -8,3 +8,14 @@ liebt(es, du).
 
 textsame(T1, T2) :- T1 == T2.
 textnotsame(T1, T2) :- T1 \= T2.
+
+pfeil(a,b).
+pfeil(a,c).
+pfeil(a,d).
+pfeil(b,c).
+pfeil(c,d).
+pfeil(c,e).
+pfeil(d,e).
+
+weg(X,Y):-pfeil(X,Y).
+weg(X,Y):-pfeil(X,Z),weg(Z,Y).
