f4aa3c12823d078a20c14873545833f99ce26198
Bruno Fernandez <brunofernandez@msn.com>
2018-06-09 09:49:59 +0200

    final commit

--- labyrinth.pl
+++ labyrinth.pl
@@ -1,40 +1,8 @@
 /**
 * Prolog project by B. Fernandez and Claudio Polo /// I3p
 * 
-* Some help from:
-* 	http://tcl.sfs.uni-tuebingen.de/~cornell/prolog/Graphs003.html
-*	https://www.cpp.edu/~jrfisher/www/prolog_tutorial/2_15.html
-*
-*	%%%%	-> wall to explode
-*	#		-> bomb to collect
-*
-*
-*	--- Map to traverse ---
-*	
-*	+--------+--------+--------+--------+--------+
-*	| START    Room2 #  Room3    Room24   Room4  |
-*	+--------+--------+--------+--------+--    --+
-*	| Room5  |                          | Room6  |
-*	+--    --+--------+--------+--------+--    --+
-*	| Room7    Room23   Room22   Room11   Room8  |
-*	+--    --+--------+--------+--    --+--    --+
-*	| Room9  |      	       | Room12   Room10 |
-*	+--    --+--------+		   +--------+--%%%%--+
-*	| Room13   Room14 |                 | Room21 |
-*	+--------+--    --+--------+        +--    --+
-*			 | Room15   End    |        | Room20 |
-*			 +--    --+--    --+--------+--    --+
-*			 | Room16 | Room17   Room18   Room19 |
-*			 +--------+--------+--------+--------+
-*
-*	The idea of this project is to find a way from room x to room y.
-*	Problems to solve ->
-*						-the rooms are bi-directed graphs -> problem of looping
-*						-police officer appears randomly in room8, room10, room11 or room12
-*						-there is a bomb that can explode a wall (%%%%)
-*
-*
-*
+* For more information please have a look at:
+* https://github.com/brunofernandez1/Artificial_Intelligence_BTI7510
 */
 
 % rooms that are connected to each other by doors
@@ -65,7 +33,6 @@ door(room19,room20).
 door(room20, room21).
 
 %guard can be on one of those rooms:
-%L = [1,2,3], random_member(A, L).
 police([room11,room8,room12,room10]).
 getPolList(X) :- police(Y), random_member(X, Y).
 
